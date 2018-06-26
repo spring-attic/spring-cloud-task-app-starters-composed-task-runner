7ab9ae6b3c2abb87c87a4c9dd71843fadcd152fd
Gunnar Hillert <ghillert@pivotal.io>
2018-06-26 11:09:36 -1000

    gh-41 Add support to interact with a secured SCDF server
    - Add additional properties
      - dataflow-server-username
      - dataflow-server-password
    - Add tests
    - Update Readme

--- spring-cloud-starter-task-composedtaskrunner/src/main/java/org/springframework/cloud/task/app/composedtaskrunner/DataFlowConfiguration.java
+++ spring-cloud-starter-task-composedtaskrunner/src/main/java/org/springframework/cloud/task/app/composedtaskrunner/DataFlowConfiguration.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 the original author or authors.
+ * Copyright 2017-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,31 +16,63 @@
 
 package org.springframework.cloud.task.app.composedtaskrunner;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.dataflow.rest.client.DataFlowTemplate;
 import org.springframework.cloud.dataflow.rest.client.TaskOperations;
+import org.springframework.cloud.dataflow.rest.util.HttpClientConfigurer;
 import org.springframework.cloud.task.app.composedtaskrunner.properties.ComposedTaskProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Configures the beans required for Connectivity to the Data Flow Server.
  *
  * @author Glenn Renfro
+ * @author Gunnar Hillert
  */
 @Configuration
 @EnableConfigurationProperties(ComposedTaskProperties.class)
 public class DataFlowConfiguration {
+	private static Log logger = LogFactory.getLog(DataFlowConfiguration.class);
 
 	@Autowired
 	private ComposedTaskProperties properties;
 
 	@Bean
 	public TaskOperations taskOperations() {
-		DataFlowTemplate dataFlowTemplate = new DataFlowTemplate(
-				this.properties.getDataflowServerUri());
+
+		final RestTemplate restTemplate = DataFlowTemplate.getDefaultDataflowRestTemplate();
+
+		validateUsernamePassword(properties.getDataflowServerUsername(),
+				properties.getDataflowServerPassword());
+		if (StringUtils.hasText(properties.getDataflowServerUsername())
+				&& StringUtils.hasText(properties.getDataflowServerPassword())) {
+			restTemplate.setRequestFactory(HttpClientConfigurer.create(this.properties.getDataflowServerUri())
+					.basicAuthCredentials(properties.getDataflowServerUsername(), properties.getDataflowServerPassword())
+					.buildClientHttpRequestFactory());
+			logger.debug("Configured basic security for accessing the Data Flow Server");
+		}
+		else {
+			logger.debug("Not configuring basic security for accessing the Data Flow Server");
+		}
+
+		final DataFlowTemplate dataFlowTemplate = new DataFlowTemplate(
+				this.properties.getDataflowServerUri(), restTemplate);
 
 		return dataFlowTemplate.taskOperations();
+	}
+
+	private void validateUsernamePassword(String userName, String password) {
+		if (!StringUtils.isEmpty(password) && StringUtils.isEmpty(userName)) {
+			throw new IllegalArgumentException("A password may be specified only together with a username");
+		}
+		if (StringUtils.isEmpty(password) && !StringUtils.isEmpty(userName)) {
+			throw new IllegalArgumentException("A username may be specified only together with a password");
+		}
 	}
 }
